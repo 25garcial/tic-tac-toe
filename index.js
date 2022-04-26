d91b94bb828fb5a56ea8b155a8292edbaeaffe56
25garcial <25garcial@camraiders.com>
2022-04-26 09:38:51 -0500

    .

--- index.js
+++ index.js
@@ -1,34 +1,72 @@
-
-
-
 const player = (name, team) => {
 
-    const markSpot = (team) => {
-        alert("")
+    const markSpot = () => {
+      return team
     };
-
-    return {name, team, markSpot}
-}
-player("player1", "X");
-player("player2", "O");
-
-
-const Board= () => {
-    let turn=true;
-    let data={s1:"",s2:"" ,s3:"" ,s4:"" ,s5:"" ,s6:"" ,s7:"" ,s8:"", s9:""}
-
-    const playTurn = () => {if (turn){player1.markSpot();}else{player2.markSpot()}}
-    return{data, turn, playTurn}
-}
-const board=Board()
-
-// init spots
-for (let i=1; i<10; i++){
-    let target=document.getElementsByClassName(`s${i}`)[0]
-    target.addEventListener("click", function(e){board.boardData[`${e.target.classList[1]}`]=e.target.classList[1];
-    alert(board.data)})
-}
-
-
-
-alert("loading succeded")
+  
+    return {
+      name,
+      team,
+      markSpot
+    }
+  }
+  const player1 = player("player1", "X");
+  const player2 = player("player2", "O");
+  
+  
+  const Board = () => {
+  
+    let turn = true;
+    let data = {
+      s1: "",
+      s2: "",
+      s3: "",
+      s4: "",
+      s5: "",
+      s6: "",
+      s7: "",
+      s8: "",
+      s9: ""
+    }
+  
+    const playTurn = () => {
+      if (turn) {
+        turn = false
+        let marker= player1.markSpot();
+  
+      } else {
+        turn = true
+        let marker= player2.markSpot()
+      }
+      //updating the board display
+      for (var i=1; i<=9; i++){
+      document.querySelector(`.s${i}`).textContent=board.data[`.s${i}`]
+      }
+      return marker
+    }
+    return {
+      data,
+      turn,
+      playTurn
+    }
+  }
+  const board = Board()
+  
+  
+  
+  // init spots
+  for (let i = 1; i < 10; i++) {
+    let target = document.getElementsByClassName(`s${i}`)[0]
+    target.addEventListener("click", function(e) {
+    let dataPosition=board.data[`${e.target.classList[1]}`]
+    if (!dataPosition){
+    board.data[`${e.target.classList[1]}`] = board.playTurn();
+    }
+    console.log(board.data)
+    })
+  }
+  
+  
+  
+  alert("loading succeded")
+  
