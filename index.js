efb19cd3d6f5c46a95af57e3495b32678f8caf1a
25garcial <91680716+25garcial@users.noreply.github.com>
2022-04-26 11:22:48 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,75 +1,83 @@
 const player = (name, team) => {
 
-    const markSpot = () => {
-      return team
-    };
-  
-    return {
-      name,
-      team,
-      markSpot
+  const markSpot = () => {
+    return team
+  };
+
+  return {
+    name,
+    team,
+    markSpot
+  }
+}
+const player1 = player("player1", "X");
+const player2 = player("player2", "O");
+
+
+const Board = () => {
+  const checkForWinner = () => {
+
+  }
+  const updateDisplay = () => {
+    for (var i = 1; i <= 9; i++) {
+      document.querySelector(`.s${i}`).textContent = board.data[`s${i}`]
+
     }
   }
-  const player1 = player("player1", "X");
-  const player2 = player("player2", "O");
-  
-  
-  const Board = () => {
-    const updateDisplay = () => {
-      for (var i = 1; i <= 9; i++) {
-        document.querySelector(`.s${i}`).textContent = board.data[`.s${i}`]
-      }
-    }
-    let turn = true;
-    let data = {
-      s1: "",
-      s2: "",
-      s3: "",
-      s4: "",
-      s5: "",
-      s6: "",
-      s7: "",
-      s8: "",
-      s9: ""
-    }
-  
-    const playTurn = () => {
-      if (turn) {
-        turn = false
-  
-        return player1.markSpot();
-  
-  
-      } else {
-        turn = true
-        return player2.markSpot()
-      }
-  
-    }
-    return {
-      data,
-      turn,
-      playTurn
-    }
+  let turn = true;
+  let data = {
+    s1: "",
+    s2: "",
+    s3: "",
+    s4: "",
+    s5: "",
+    s6: "",
+    s7: "",
+    s8: "",
+    s9: ""
   }
-  const board = Board()
-  
-  
-  
-  // init spots
-  for (let i = 1; i < 10; i++) {
-    let target = document.getElementsByClassName(`s${i}`)[0]
-    target.addEventListener("click", function(e) {
-      let dataPosition = board.data[`${e.target.classList[1]}`]
-      if (!dataPosition) {
-        board.data[`${e.target.classList[1]}`] = board.playTurn();
-      }
-      board.updateDisplay()
-      console.log(board.data)
-    })
+
+  const playTurn = () => {
+    if (turn) {
+      turn = false
+
+      return player1.markSpot();
+
+
+    } else {
+      turn = true
+      return player2.markSpot()
+    }
+
   }
-  
-  
-  
-  alert("loading succeded")
-  
+  return {
+    data,
+    turn,
+    updateDisplay,
+    playTurn,
+    checkForWinner
+  }
+}
+const board = Board()
+
+
+
+// init spots
+for (let i = 1; i < 10; i++) {
+  let target = document.getElementsByClassName(`s${i}`)[0]
+  target.addEventListener("click", function(e) {
+    let dataPosition = board.data[`${e.target.classList[1]}`]
+    if (!dataPosition) {
+      board.data[`${e.target.classList[1]}`] = board.playTurn();
+    }
+
+    board.updateDisplay()
+    console.log(board.data)
+    console.log(e.target.textContent)
+
+  })
+}
+
+
+
+alert("loading succeded")
