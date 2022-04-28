f376d37305a8d851c39f14956d602840c8d474f2
25garcial <25garcial@camraiders.com>
2022-04-28 09:39:05 -0500

    .

--- index.js
+++ index.js
@@ -15,27 +15,21 @@ const player2 = player("player2", "O");
 
 
 const Board = () => {
-  const checkForWinner = () => {
-    if (board.data[0] == board.data[1] == board.data[2] || board.data[0] == board.data[4] == board.data[8] || board.data[0] == board.data[3] == board.data[6] || board.data[3] == board.data[4] && board.data[5] || board.data[6] == board.data[7] == board.data[8] || board.data[2] == board.data[5] == board.data[8]) {
-      if (turn) {
-        console.log("player 1 wins")
-        document.querySelector(".result").textContent = "Player 1 wins."
-      } else {
-        console.log("player 2 wins")
-        document.querySelector(".result").textContent = "Player 2 wins."
-      }
+  function win(){
+    if (!turn) {
+      console.log("player 1 wins")
+      document.querySelector(".result").textContent = "Player 1 wins."
+    } else {
+      console.log("player 2 wins")
+      document.querySelector(".result").textContent = "Player 2 wins."
     }
-
-
-
-
-
-
-
+  }
+  const checkForWinner = () => {
+    if (data["s1"]==data["s2"] && data["s2"]==data["s3"] && data["s3"]!=="") {win()}
   }
   const updateDisplay = () => {
     for (var i = 1; i <= 9; i++) {
-      document.querySelector(`.s${i}`).textContent = board.data[`s${i}`]
+      document.querySelector(`.s${i}`).textContent = data[`s${i}`]
 
     }
   }
@@ -54,13 +48,11 @@ const Board = () => {
 
   const playTurn = () => {
     if (turn) {
-      turn = false
-
+      turn = !turn
       return player1.markSpot();
 
-
     } else {
-      turn = true
+      turn = !turn
       return player2.markSpot()
     }
 
@@ -89,7 +81,6 @@ for (let i = 1; i < 10; i++) {
     board.updateDisplay()
     board.checkForWinner()
     console.log(board.data)
-    console.log(e.target.textContent)
 
   })
 }
