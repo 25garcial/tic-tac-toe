6cafaa1ed76be4eed69ce32e86e86eecadb298a9
25garcial <91680716+25garcial@users.noreply.github.com>
2022-04-28 11:56:54 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
+let winner=false
+function restart(){
+for (var i=0; i<=9; i++){
+board.data[`s${i}`]=""
+document.querySelector(".result").textContent="result:"
+board.updateDisplay()
+winner=0
+board.turn=true
+}
+}
+function innitSpots(){
+for (let i = 1; i < 10; i++) {
+  let target = document.getElementsByClassName(`s${i}`)[0]
+  target.addEventListener("click", function(e) {
+  if (!winner){
+    let dataPosition = board.data[`${e.target.classList[1]}`]
+    alert(!dataPosition)
+    if (!dataPosition) {
+      board.data[`${e.target.classList[1]}`] = playTurn();
+      board.checkForWinner()
+    }
+    
+    console.log(board.data)
+
+  }})
+}}
 function playTurn() {
   if (board.turn) {
     return player1.team
@@ -18,6 +44,12 @@ const player2 = player("player2", "O");
 
 
 const Board = () => {
+const updateDisplay = () => {
+    for (var i = 1; i <= 9; i++) {
+      document.querySelector(`.s${i}`).textContent = data[`s${i}`]
+    }
+    board.turn = !board.turn
+  }
   function win() {
     if (turn) {
       console.log("player 1 wins")
@@ -26,8 +58,10 @@ const Board = () => {
       console.log("player 2 wins")
       document.querySelector(".result").textContent = "Player 2 wins."
     }
-  }
+    winner=true
+ }
   const checkForWinner = () => {
+  updateDisplay()
     if (data["s1"] == data["s2"] && data["s2"] == data["s3"] && data["s3"] !== "") {
       win()
     }
@@ -52,14 +86,9 @@ const Board = () => {
     if (data["s3"] == data["s5"] && data["s5"] == data["s7"] && data["s7"] !== "") {
       win()
     }
-
+	turn=!turn
   }
-  const updateDisplay = () => {
-    for (var i = 1; i <= 9; i++) {
-      document.querySelector(`.s${i}`).textContent = data[`s${i}`]
-    }
-    board.turn = !board.turn
-  }
+  
   let turn = true;
   let data = {
     s1: "",
@@ -86,17 +115,5 @@ const board = Board()
 
 
 // init spots
-for (let i = 1; i < 10; i++) {
-  let target = document.getElementsByClassName(`s${i}`)[0]
-  target.addEventListener("click", function(e) {
-    let dataPosition = board.data[`${e.target.classList[1]}`]
-    if (!dataPosition) {
-      board.data[`${e.target.classList[1]}`] = playTurn();
-    }
 
-    board.updateDisplay()
-    board.checkForWinner()
-    console.log(board.data)
-
-  })
-}
+innitSpots()
